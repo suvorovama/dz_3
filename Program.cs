b5acd8cc50068789f67ef657146e2159cc098bcf
margarita s <runlet@inbox.ru>
2022-08-15 11:35:31 +0300

    First program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите пятизначное число:");
+string? number = Console.ReadLine();
+if (number[0] == number[4] && number[1] == number[3])
+{
+    Console.Write ("да");
+}
+else
+{
+    Console.Write ("нет");
+}
